af8171bb05a398da734d03a1300bb8a97c0bd0cd
Olivier BOVIER <olivier.bovier@free.fr>
2018-04-06 14:15:24 +0200

    Modifs Olivier début PM

---
+++ page2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Page 2</title>
+	<style type="text/css">
+		h1{
+			position: absolute;
+			top: 40%;
+			left: 40%;
+			font-size: 50px;
+			color: purple;
+		}
+
+
+	</style>
+</head>
+
+<body>
+	<h1>Page 2</h1>
+	
+
+
+</body>
+
+</html>
